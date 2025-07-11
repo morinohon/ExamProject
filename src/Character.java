955bd6ef190b214bd19ebdf49c80f4a3d9734966
MorimotoYura <241103@std.hi-joho.ac.jp>
2025-07-11 12:30:30 +0900

    課題が途中

--- src/Character.java
+++ src/Character.java
@@ -1,9 +1,24 @@
+import java.io.*;
+
 public abstract class Character implements Creature{
+    private String name;
+    private int hp;
+
     public void name(String name){
     }
-    public void hp(int hp){
+    private void getHp(int hp){
         if(hp <= 0){
-            throw new IllegalArgumentExceptionthrow("初期設定に誤りがあるため、キャラクターを作成できませんでした");
+            throw new IllegalArgumentException("初期設定に誤りがあるため、キャラクターを作成できませんでした");
         }
+        this.hp = hp;
+    }
+    public final boolean isAlive(){
+        return getHp() > 0;
+    }
+    public void showStatus(String name, int hp){
+        System.out.println("[" + this.name + "]" + ":HP[" + this.hp + "]" );
+    }
+    private void setHp(){
+        this.hp = hp;
     }
 }
--- src/Creature.java
+++ src/Creature.java
@@ -1,24 +1,8 @@
 public interface Creature {
-    void name();
-    void hp();
-
-    public static boolean isAlive(){
-        return true;
-    }
-    /*public void showStatus(){
-    }*/
-
-    public default void showStatus(){
-    }
-    public default void attack(Creature target){
-    }
-    public default String getName(){
-        return this.name();
-    }
-    public default int getHp(){
-        return this.hp();
-    }
-    public default void setHp(int hp) {
-        this.hp() = hp;
-    }
+    public boolean isAlive();
+    public void showStatus();
+    public void attack(Creature target);
+    public String getName();
+    public int getHp();
+    public int setHp(int hp);
 }
